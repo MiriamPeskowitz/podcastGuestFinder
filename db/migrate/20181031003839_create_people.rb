ea22d175346a02a9877529812148182cac0be9fa
MiriamPeskowitz <miriam5000@gmail.com>
2018-10-30 20:52:09 -0400

    add null: false

--- db/migrate/20181031003839_create_people.rb
+++ db/migrate/20181031003839_create_people.rb
@@ -1,7 +1,7 @@
 class CreatePeople < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end

aea71d5d13fac662257ab12cb1ef45f17d302be3
MiriamPeskowitz <miriam5000@gmail.com>
2018-10-31 15:00:17 -0400

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2018_10_31_003839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  # enable_extension "pg_trgm"
 
   create_table "people", force: :cascade do |t|
     t.string "name", null: false
